93ac74132d683577b4bf70556f9617b4e9197a58
tedelcopp <tomas.edelcopp@gmail.com>
2025-08-28 19:44:07 -0300

    WIP.

--- medapp/server.js
+++ medapp/server.js
@@ -6,17 +6,10 @@ import routes from "./routes/index.js"; // ojo la extensión .js
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-<<<<<<< HEAD
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-=======
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-
-app.use(cors()); 
->>>>>>> 542862e2a415e58803ec6e149c5c7048aecf7b2e
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
