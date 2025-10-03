a4b1df422f077c9749b5909de8f73f7776bdc621
tedelcopp <tomas.edelcopp@gmail.com>
2025-10-03 11:33:36 -0300

    WIP.

--- medapp/config/db.js
+++ medapp/config/db.js
@@ -1,11 +1,7 @@
 import "dotenv/config";
 import mysql from "mysql2/promise";
-import fs from "fs";
-import path from "path";
 
-const certPath = path.join(__dirname, "certs", "isrgroot1.pem");
-
-const TlsCert = fs.readFileSync(certPath);
+const TlsCert = process.env.TIDB_CA_CERT;
 
 const pool = mysql.createPool({
   uri: process.env.DATABASE_URL,
