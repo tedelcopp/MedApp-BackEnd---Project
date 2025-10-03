de9aeff8296cd2629745415052447b8dcfbe9bb0
tedelcopp <tomas.edelcopp@gmail.com>
2025-10-03 11:07:36 -0300

    DB.WIP

--- medapp/config/db.js
+++ medapp/config/db.js
@@ -2,14 +2,15 @@ import "dotenv/config";
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  uri: process.env.DATABASE_URL,
+
   ssl: {
     rejectUnauthorized: true,
   },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 pool.on("error", (err) => {
