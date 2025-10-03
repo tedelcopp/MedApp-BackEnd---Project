b72aca1c432834fef42b9c713c88fd296ff6dd13
tedelcopp <tomas.edelcopp@gmail.com>
2025-10-03 10:04:01 -0300

    ConecctionDB.WIP

--- medapp/config/db.js
+++ medapp/config/db.js
@@ -1,26 +1,3 @@
-// import mysql from "mysql2";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST || "localhost",
-//   user: process.env.DB_USER || "root",
-//   password: process.env.DB_PASSWORD || "123456",
-//   database: process.env.DB_NAME || "medapp",
-//   port: process.env.DB_PORT || 3306,
-// });
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error("❌ Error al conectar a la base de datos:", err);
-//     return;
-//   }
-//   console.log("✅ Conexión exitosa a MySQL");
-// });
-
-// export default connection;
-
 import "dotenv/config";
 import mysql from "mysql2/promise";
 
@@ -30,6 +7,14 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: true,
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 pool.on("error", (err) => {
