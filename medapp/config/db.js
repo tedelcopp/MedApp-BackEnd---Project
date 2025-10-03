e4eb3b4a07d7e53fc206185393da8d25c1e77e8a
tedelcopp <tomas.edelcopp@gmail.com>
2025-10-03 10:08:19 -0300

    ConnectionDB.WIP2

--- medapp/config/db.js
+++ medapp/config/db.js
@@ -7,14 +7,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-
   ssl: {
     rejectUnauthorized: true,
   },
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 pool.on("error", (err) => {
